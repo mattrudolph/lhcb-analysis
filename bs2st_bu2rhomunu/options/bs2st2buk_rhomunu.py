472e3af56da320667d369a01b44333da95ac551a
Matthew S Rudolph <matthewscottrudolph@gmail.com>
2015-11-20 15:16:59 -0500

    rhomunu options files

---
+++ bs2st_bu2rhomunu/options/bs2st2buk_rhomunu.py
@@ -0,0 +1,22 @@
+from Gaudi.Configuration import *
+
+from Configurables import DaVinci,GaudiSequencer
+
+DaVinci().DataType = "2012"
+DaVinci().EvtMax = -1
+DaVinci().TupleFile = "bs2st2buk_rhomunu.root"
+
+from bs2st_bu2rhomunu.options.rhomuselection import *
+from bs2st_bu2rhomunu.options.rhomutuples import *
+
+tuple.Inputs = [ seqBss2buk.outputLocation() ]
+tupleSS.Inputs = [ seqBss2bukSS.outputLocation() ]
+
+SequenceOS = GaudiSequencer('SequenceOS')
+SequenceSS = GaudiSequencer('SequenceSS')
+
+SequenceOS.Members = [ seqBss2buk.sequence(), tuple ]
+SequenceSS.Members = [ seqBss2bukSS.sequence(), tupleSS ]
+
+
+DaVinci().UserAlgorithms = [ SequenceOS, SequenceSS ]
---
+++ bs2st_bu2rhomunu/options/bs2st2buk_rhomunu_local.py
@@ -0,0 +1,7 @@
+from bs2st_bu2rhomunu.options.bs2st2buk_rhomunu import *
+
+from Configurables import DaVinci
+
+DaVinci().EvtMax = -1
+
+DaVinci().Input = ['input/00041834_00052925_1.semileptonic.dst']

a0ac794bc91db1edfd22c0153a7235b33ff06701
Matt Rudolph <matthew.scott.rudolph@gmail.com>
2015-12-07 12:03:11 -0500

    commit

---
+++ bs2st_bu2rhomunu/run/submit.py
@@ -0,0 +1,30 @@
+"""
+Submit bs2st2buk_kmux options 
+
+"""
+
+class Submit(Submitter):
+    def __init__(self):
+        Submitter.__init__(self)
+        
+    #Small set so lets change the splitting
+    def modJob(self,j):
+        j.splitter.filesPerJob=20
+
+
+
+        
+kmux = Submit()
+
+#Modify to determine what to run on; label of dataset in box and isMC flag
+kmux.dataToRun = [("SEMILEPTONIC.DST 2011 strip 21 MagUp",False),
+                  ("SEMILEPTONIC.DST 2011 strip 21 MagUp",False),
+]
+
+#base options to run
+kmux.options = "../options/bs2st2buk_rhomunu.py"
+
+#output ntuple file
+kmux.outputs = ["bs2st2buk_rhomunu.root",]
+
+kmux.submit()
